d11d53ef051355fbfc696953df53e6413770e6ad
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-04-28 21:22:23 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -27,12 +27,13 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""Hello 👋 there! I can play music in voice chats of Telegeam Groups. I have a lot of cool feature that will amaze you!\n\n🔴 Do you want me to play music in your Telegram groups'voice chats? Please click the \'📜 User Manual 📜\' button below to know how you can use me.\n\n🔴 The Assistant must be in your group to play music in the voice chat of your group.\n\n🔴 More info & commands mentioned in the [User Manual](https://telegra.ph/Daisy-X-04-19)\n\nA project by @TeamDaisyX""",
+        f"""Hello 👋 there! I can play music in voice chats of Telegeam Groups. I have a lot of cool feature that will amaze you!\n\n🔴 Do you want me to play music in your Telegram groups'voice chats? Please click the \'📜 User Manual 📜\' button below to know how you can use me.\n\n🔴 The Assistant must be in your group to play music in the voice chat of your group.\n\n🔴 More info & commands mentioned in the [User Manual](
+        )\n\nA project by @TeamDaisyX""",
         reply_markup=InlineKeyboardMarkup(
             [ 
                 [
                     InlineKeyboardButton(
-                        "📜 User Manual 📜", url="https://telegra.ph/Daisy-X-04-19")
+                        "📜 User Manual 📜", url="https://telegra.ph/Yeagerist-Music-Streamer-Bot-04-28")
                   ],[
                     InlineKeyboardButton(
                         "👨‍💻 Updates 👨‍💻", url="https://t.me/daisyxupdates"
